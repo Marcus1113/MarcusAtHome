0e74d8e7d966a08069bf7a7b798799aceca79fb2
Marcus-Office <marcus@techninier.com>
2022-12-02 17:31:49 +0800

    + update layout

---
+++ resources/views/web/_template/header.blade.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-light bg-light shadow">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Marcus At Home</a>
+        <img src="https://thumbs.gfycat.com/PlayfulBraveHarvestmouse.webp" alt="" style="max-width: 60px">
+    </div>
+</nav>
---
+++ resources/views/web/_template/template.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marcus At Home</title>
+
+    {{-- BOOTSRAP --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    @yield('afterStyle')
+</head>
+<body>
+    @include('web._template.header')
+    <div class="content pt-3" style="min-height: 900px">
+        @yield('content')
+    </div>
+    @include('web._template.footer')
+
+    {{-- JQUERY --}}
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    {{-- BOOTSRAP --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    @yield('afterScript')
+</body>
+</html>
